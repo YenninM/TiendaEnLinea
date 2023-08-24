d0a69f6bc65fdbf3142c09302c1e000e5cc21fdc
YenninM <yarethgracia@gmail.com>
2023-08-24 15:19:23 -0700

    Implementado bootstrap

---
+++ E-commerce/backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: "Excavadora",
+      slug: "Excavadora",
+      categoria: "Maquinaria",
+      image: "/images/e1.jpg",
+      price: 20,
+      disponible: 5,
+      marca: "Marca",
+      calificacion: 5,
+      valoraciones: 10,
+      descripcion: "excavadora de color amarillo",
+    },
+    {
+      name: "libros de historia",
+      slug: "libro",
+      categoria: "libros",
+      image: "/images/l1.jpg",
+      price: 30,
+      disponible: 5,
+      marca: "ghandi",
+      calificacion: 5,
+      valoraciones: 10,
+      descripcion: "libro de historia del banco de mexico",
+    },
+    {
+      name: "adorno",
+      slug: "adorno",
+      categoria: "decoracion",
+      image: "/images/d1.jpg",
+      price: 30,
+      disponible: 5,
+      marca: "madera",
+      calificacion: 5,
+      valoraciones: 10,
+      descripcion: "Lindo adorno de madera",
+    },
+  ],
+};
+
+export default data;
--- E-commerce/frontend/src/App.js
+++ E-commerce/frontend/src/App.js
@@ -1,11 +1,36 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/NavBar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+
 function App() {
   return (
-    <div>
-      <header>
-        <a href='/'>TiendaEnLinea</a>
-      </header>
-      <main>Lista de Productos</main>
-    </div>
+    <BrowserRouter>
+      <div className='d-flex flex-column site-container'>
+        <header>
+          <Navbar bg='dark' variant='dark'>
+            <Container>
+              <LinkContainer to='/'>
+                <Navbar.Brand>TiendaEnLinea</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path='/product/:slug' element={<ProductScreen />} />
+              <Route path='/' element={<HomeScreen />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className='text-center'>Tienda en Linea</div>
+        </footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
