f8d3b1f3c99058474876507ab1c06e08a434f77a
Yennin Muñoz <47438975+YenninM@users.noreply.github.com>
2023-11-28 20:29:33 -0700

    prueba login 2

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,8 @@ const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String},
+    password: { type: String, required: true },
+    resetToken: { type: String },
     isAdmin: { type: Boolean, default: false, required: true },
   },
   {
